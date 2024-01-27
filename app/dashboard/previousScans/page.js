f58a13c5a2684fb91b8ee72fd8f7ccced805c4aa
Amjad Ali <amjadali.eth@gmail.com>
2024-01-27 15:15:07 +0530

    all sensors completed in previous scans page

--- app/dashboard/previousScans/page.js
+++ app/dashboard/previousScans/page.js
@@ -15,6 +15,7 @@ export default function Page() {
   const [filteredReadings, setFilteredReadings] = useState({});
   const [selectedDate, setSelectedDate] = useState("");
   const [selectedTime, setSelectedTime] = useState("");
+  const [selectedSensor, setSelectedSensor] = useState("allreadings");
 
   const sensorHeadings = {
     bp: "Blood Pressure",
@@ -49,9 +50,9 @@ export default function Page() {
       newStructuredReadings[sensor] = {};
 
       readings[sensor].forEach((reading) => {
-        if (reading.readings === undefined || reading.readings === 'undefined') {
+        if (reading.readings === undefined || reading.readings === 'undefined' || reading.readings === 'undefined/undefined') 
           return;
-        }
+
         const formattedDate = moment(
           reading.timestamp,
           "MMM DD, YYYY at hh:mm:ss A Z"
@@ -76,48 +77,115 @@ export default function Page() {
       });
     });
 
+    const sortedDates = Array.from(dates).sort();
+    if (sortedDates.length > 0) {
+      setSelectedDate(sortedDates[0]);
+
+      const times =
+        newStructuredReadings[Object.keys(newStructuredReadings)[0]][
+          sortedDates[0]
+        ];
+      if (times) {
+        const sortedTimes = Object.keys(times).sort();
+        if (sortedTimes.length > 0) {
+          setSelectedTime(sortedTimes[0]);
+        }
+      }
+    }
     setUniqueDates(dates);
     setFilteredReadings(newStructuredReadings);
-    console.log(newStructuredReadings)
   }
 
+  //
+  const updateSelectedSensor = (sensorKey) => {
+    setSelectedSensor(sensorKey);
+
+    if ( filteredReadings[sensorKey]) {
+      const dates = Object.keys(filteredReadings[sensorKey]).sort();
+      if (dates.length > 0) {
+        const firstDate = dates[0];
+        setSelectedDate(firstDate);
+
+        const times = Object.keys(filteredReadings[sensorKey][firstDate]).sort();
+        if (times.length > 0) {
+          setSelectedTime(times[0]);
+        } else {
+          setSelectedTime("");
+        }
+      } else {
+        setSelectedDate("");
+        setSelectedTime("");
+      }
+    }
+  };
+
+  //
+
+  //
   function renderTimeButtons() {
     if (!selectedDate || !filteredReadings) return null;
-  
+
     let times = new Set();
-    Object.keys(filteredReadings).forEach((sensorKey) => {
-      const dateReadings = filteredReadings[sensorKey][selectedDate];
-      if (dateReadings) {
-        Object.entries(dateReadings).forEach(([time, readings]) => {
-          if (readings.length > 0) {
+    if (selectedSensor === "allreadings") {
+      Object.keys(filteredReadings).forEach((sensorKey) => {
+        const dateReadings = filteredReadings[sensorKey][selectedDate];
+        if (dateReadings) {
+          Object.keys(dateReadings).forEach((time) => {
             times.add(time);
-          }
+          });
+        }
+      });
+    } else {
+      const dateReadings = filteredReadings[selectedSensor][selectedDate];
+      if (dateReadings) {
+        Object.keys(dateReadings).forEach((time) => {
+          times.add(time);
         });
       }
-    });
-  
+    }
+
     return Array.from(times).map((time) => (
       <Button
         key={time}
         className=" text-xs px-2 cursor-pointer h-6 rounded-lg"
         onClick={() => setSelectedTime(time)}
+        variant = { time === selectedTime ? 'default' : 'secondary'}
       >
         {time}
       </Button>
     ));
   }
-  
+
+  //
+
+  //triggers
+  function renderTabsTriggers() {
+    return Object.keys(sensorHeadings).map((sensorKey) => (
+      <TabsTrigger
+        key={sensorKey}
+        value={sensorKey}
+        onClick={() => updateSelectedSensor(sensorKey)}
+      >
+        {sensorHeadings[sensorKey]}
+      </TabsTrigger>
+    ));
+  }
+  //
 
   //cards rendering
-  function renderReadingCards() {
+  function renderAllReadings() {
     if (!selectedDate || !selectedTime || !filteredReadings) return null;
 
     const sensorMap = {};
 
     Object.keys(filteredReadings).forEach((sensorKey) => {
       const dateReadings = filteredReadings[sensorKey][selectedDate];
-    if (dateReadings && dateReadings[selectedTime] && dateReadings[selectedTime].length > 0) {
-      const sensorReadings = dateReadings[selectedTime];
+      if (
+        dateReadings &&
+        dateReadings[selectedTime] &&
+        dateReadings[selectedTime].length > 0
+      ) {
+        const sensorReadings = dateReadings[selectedTime];
 
         sensorMap[sensorKey] = sensorReadings.reduce((acc, reading) => {
           let isInvalidReading =
@@ -132,7 +200,6 @@ export default function Page() {
             "MMM DD, YYYY at hh:mm:ss A Z"
           ).format("h:mm A");
 
-          // Store only the latest reading for each timestamp
           acc[formattedTimestamp] = { ...reading, formattedTimestamp };
           return acc;
         }, {});
@@ -147,15 +214,12 @@ export default function Page() {
         <h4 className="text-sm text-gray-400 mb-2">
           {sensorHeadings[sensorKey]}
         </h4>
-        <div className="flex overflow-x-auto gap-8">
+        <div className="flex  gap-8">
           {Object.values(sensorMap[sensorKey]).map((reading, index) => (
             <div key={index} className="flex-none">
               <p className="text-3xl text-bluetext font-bold">
                 {parseFloat(reading.readings).toFixed(2)}{" "}
                 <span className="text-sm">{sensorunit[sensorKey]}</span>
-              </p>
-              <p className="text-xs text-gray-600 font-semibold">
-                {reading.formattedTimestamp}
               </p>
             </div>
           ))}
@@ -164,6 +228,68 @@ export default function Page() {
     ));
   }
 
+  //
+
+  //
+  function renderReadings(sensorKey) {
+
+    if (!selectedDate || !selectedTime || !filteredReadings) {
+      return (
+        <div>
+          Oops ! No data recorded yet
+        </div>
+      )
+    }
+
+    const sensorMap = {};
+
+    const dateReadings = filteredReadings[sensorKey][selectedDate];
+    if (
+      dateReadings &&
+      dateReadings[selectedTime] &&
+      dateReadings[selectedTime].length > 0
+    ) {
+      const sensorReadings = dateReadings[selectedTime];
+
+      sensorMap[sensorKey] = sensorReadings.reduce((acc, reading) => {
+        let isInvalidReading =
+          reading.readings === undefined ||
+          (typeof reading.readings === "string" &&
+            reading.readings.includes("undefined"));
+
+        if (isInvalidReading) return acc;
+
+        const formattedTimestamp = moment(
+          reading.timestamp,
+          "MMM DD, YYYY at hh:mm:ss A Z"
+        ).format("h:mm A");
+
+        acc[formattedTimestamp] = reading;
+        return acc;
+      }, {});
+    }
+
+    return Object.keys(sensorMap).map((sensorKey) => (
+      <div
+        key={sensorKey}
+        className="mb-4 bg-white shadow-lg shadow-gray-100 rounded-xl p-4"
+      >
+        <h4 className="text-sm text-gray-400 mb-2">
+          {sensorHeadings[sensorKey]}
+        </h4>
+        <div className="flex  gap-8">
+          {Object.values(sensorMap[sensorKey]).map((reading, index) => (
+            <div key={index} className="flex-none">
+              <p className="text-3xl text-bluetext font-bold">
+                {parseFloat(reading.readings).toFixed(2)}{" "}
+                <span className="text-sm">{sensorunit[sensorKey]}</span>
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    ));
+  }
   //
 
   return (
@@ -178,9 +304,13 @@ export default function Page() {
       <div className="bg-white h-[80%] rounded-lg p-2 shadow-lg shadow-gray-100">
         <Tabs defaultValue="allreadings" className="w-full flex gap-4 h-full">
           <TabsList className="flex flex-col w-max gap-2 h-full items-start justify-start bg-lightblue text-bluetext">
-            <TabsTrigger value="allreadings">All Readings</TabsTrigger>
-            <TabsTrigger value="bt">Body Temperature</TabsTrigger>
-            <TabsTrigger value="sp02">Sp02</TabsTrigger>
+            <TabsTrigger
+              value="allreadings"
+              onClick={() => updateSelectedSensor("allreadings")}
+            >
+              All Readings
+            </TabsTrigger>
+            {renderTabsTriggers()}
           </TabsList>
           <div className="flex flex-col w-full h-full">
             <div className="flex overflow-x-auto h-[3rem] w-full justify-between items-center gap-4">
@@ -203,9 +333,19 @@ export default function Page() {
             </div>
             <TabsContent value="allreadings">
               <div className="flex justify-between flex-col bg-[#F9F9F9] rounded-xl p-2 gap-4 overflow-y-auto h-[20rem]">
-                {renderReadingCards()}
+                {renderAllReadings()}
               </div>
             </TabsContent>
+
+            {Object.keys(filteredReadings).map((sensorKey) => {
+              return (
+                <TabsContent key={sensorKey} value={sensorKey}>
+                  <div className="flex justify-between flex-col bg-[#F9F9F9] rounded-xl p-2 gap-4 overflow-y-auto h-[20rem]">
+                    {renderReadings(sensorKey)}
+                  </div>
+                </TabsContent>
+              );
+            })}
 
             <TabsContent value="bt">Bt</TabsContent>
           </div>
