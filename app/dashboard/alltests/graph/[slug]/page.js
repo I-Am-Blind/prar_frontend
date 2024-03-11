4a8ef371ea105cef03fb0b3ec3734165805dd675
Amjad Ali <amjadali.eth@gmail.com>
2024-03-11 20:36:34 +0530

    added rechartjs and customized tips

--- app/dashboard/alltests/graph/[slug]/page.js
+++ app/dashboard/alltests/graph/[slug]/page.js
@@ -11,11 +11,20 @@ import right_arrow from "@/public/assets/right_arrow_white.svg";
 import left_arrow from "@/public/assets/left_small_arrow_black.svg";
 import initiate_test from "@/public/assets/initiate_test.png";
 import Image from "next/image";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 export default function Page({ params }) {
   const router = useRouter();
   const chartRef = useRef(null); // Ref for chart instance
-  const chartContainerRef = useRef(null); // Ref for the canvas container
+  const [data, setData] = useState([]);
 
   const slugtourl = {
     bp: "bloodpressure",
@@ -29,124 +38,92 @@ export default function Page({ params }) {
     async function getlastreadings() {
       const readingsData = await GetReadings(params.slug);
       const readings = readingsData[params.slug];
-      let labels = [];
-      let data = [];
-      let data1 = [];
-      readings.forEach((reading) => {
-        const timestamp = moment(
-          reading.timestamp,
-          "MMM DD, YYYY at h:mm:ss A Z"
-        ).format("lll");
-        labels.push(timestamp);
+      const formattedData = readings.map(reading => {
+        const timestamp = moment(reading.timestamp, "MMM DD, YYYY at h:mm:ss A Z").valueOf(); // Convert to a valueOf for Recharts
         if (params.slug !== "bp") {
-          data.push(reading.readings);
+          return { timestamp, value: reading.readings };
         } else {
-          const [systolic, diastolic] = reading.readings
-            .split("/")
-            .map((value) => parseInt(value, 10));
-
-          if (typeof systolic === "number") data.push(systolic);
-          if (typeof diastolic === "number") data1.push(diastolic);
+          const [systolic, diastolic] = reading.readings.split("/").map(value => parseInt(value, 10));
+          return { timestamp, systolic, diastolic };
         }
       });
-
-      if (!chartContainerRef.current) return;
-
-      const ctx = chartContainerRef.current.getContext("2d");
-      if (chartRef.current) {
-        chartRef.current.destroy(); // Destroy the previous instance if it exists
-      }
-
-      let datasets;
-      if (params.slug !== "bp") {
-        datasets = [
-          {
-            data: data, // Readings for the y-axis
-            label: "Readings",
-            borderColor: "#3e95cd",
-            backgroundColor: "#7bb6dd",
-            fill: false,
-          },
-        ];
-      } else {
-        datasets = [
-          {
-            data: data, // Readings for the y-axis
-            label: "Systolic",
-            borderColor: "#3e95cd",
-            backgroundColor: "#7bb6dd",
-            fill: false,
-          },
-          {
-            data: data1, // Readings for the y-axis
-            label: "Diastolic",
-            borderColor: "#000000",
-            backgroundColor: "#000000",
-            fill: false,
-          },
-        ];
-      }
-      console.log(labels);
-      chartRef.current = new Chart(ctx, {
-        type: "line",
-        data: {
-          labels,
-          datasets: datasets,
-        },
-        options: {
-          scales: {
-            x: {
-              type: "time",
-              time: {
-                parser: "lll", // Adjust if necessary to match the input format
-                tooltipFormat: "ll HH:mm", // Display hours and minutes in tooltip
-                unit: "hour", // Display labels in hourly increments
-                displayFormats: {
-                  hour: "MMM D, hA", // Customize as needed (e.g., 'hA' for hourly labels like '3PM')
-                },
-              },
-              ticks: {
-                autoSkip: true, // Let Chart.js skip labels to prevent overlap
-                maxRotation: 45, // Keep labels horizontal
-                maxTicksLimit: 15, // Adjust based on the size of your chart
-              },
-            },
-            y: {
-              type: "linear",
-              min: 0,
-              max: 200,
-              beginAtZero: true,
-            },
-          },
-        },
-      });
+      setData(formattedData);
     }
+
     getlastreadings();
   }, [params.slug]);
 
-  return (
-    <main className="w-screen h-screen pt-10 px-4 flex flex-col gap-2 items-start bg-slate-50  ">
-        <div className="flex justify-center items-center">
-          <Image alt="initiate" src={left_arrow} className="w-6"/>
-          <h2 className="ml-4 text-lg capitalize font-semibold text-darkblue">{slugtourl[params.slug]}</h2>
-        </div>
-        <Button
-          className="h-[7rem] w-52 flex flex-col gap-2 items-end bg-green1"
-          onClick={() => {
-            router.push("/dashboard/alltests/" + slugtourl[params.slug]);
+  const CustomTooltip = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+      return (
+        <div
+          className="custom-tooltip"
+          style={{
+            backgroundColor: "#ffff",
+            padding: "5px",
+            border: "1px solid #cccc",
           }}
         >
-          <Image alt="initiate" src={initiate_test} className="w-10" />
-          <div className="flex justify-between w-full">
-            <span className="text-left">
-              Initiate <br /> test{" "}
-            </span>
+          <p className="label">{`Time: ${moment(label).format("lll")}`}</p>
+          <p className="intro">{`Value: ${payload[0].value}`}</p>
+          {payload[1] && (
+            <p className="intro">{`Value: ${payload[1].value}`}</p>
+          )}
+        </div>
+      );
+    }
 
-            <Image alt="initiate" src={right_arrow} className="" />
-          </div>
-        </Button>
+    return null;
+  };
+
+  return (
+    <main className="w-screen h-screen pt-10 px-4 flex flex-col gap-2 items-start bg-slate-50  ">
+      <div className="flex justify-center items-center">
+        <Image alt="initiate" src={left_arrow} className="w-6" />
+        <h2 className="ml-4 text-lg capitalize font-semibold text-darkblue">
+          {slugtourl[params.slug]}
+        </h2>
+      </div>
+      <Button
+        className="h-[7rem] w-52 flex flex-col gap-2 items-end bg-green1"
+        onClick={() => {
+          router.push("/dashboard/alltests/" + slugtourl[params.slug]);
+        }}
+      >
+        <Image alt="initiate" src={initiate_test} className="w-10" />
+        <div className="flex justify-between w-full">
+          <span className="text-left">
+            Initiate <br /> test{" "}
+          </span>
+
+          <Image alt="initiate" src={right_arrow} className="" />
+        </div>
+      </Button>
       <div className="w-full h-[23rem]  pt-0 rounded-xl shadow-lg p-4 shadow-gray-200 bg-white">
-        <canvas id="myChart" ref={chartContainerRef} className="h-full w-full"></canvas>
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart
+            data={data}
+            margin={{
+              top: 20,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" vertical={false}/>
+            <XAxis
+              dataKey="timestamp"
+              tickFormatter={(timeStr) => moment(timeStr).format("MMM D")}
+            />
+            <YAxis />
+            <Tooltip content={<CustomTooltip />} />
+            <Line
+              type="monotone"
+              dataKey={params.slug !== "bp" ? "value" : "systolic"}
+              stroke="#8884d8"
+            />
+            {params.slug === "bp" && (
+              <Line type="monotone" dataKey="diastolic" stroke="#82ca9d" />
+            )}
+          </LineChart>
+        </ResponsiveContainer>
       </div>
     </main>
   );
